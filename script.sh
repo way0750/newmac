a7fac0c687b4dda3ef6392fa40f5ff13e9c47216
Huiqiang (Way) Huang <bruce0750@gmail.com>
2019-12-01 03:09:51 -0800

    add new script

--- script.sh
+++ script.sh
@@ -1,24 +1,37 @@
-# things to install
+# install oh my zsh first because some of the following depends on my zsh’s dir
+# make when done installing zsh, exit out of it to continue
+mkdir ~/repos
+
+sh ./gitInstallations/oh-my-zsh/tools/install.sh
+
+# should find+replace and/or add new alias and whatnot to .zshrc
+
+# install fonts:
+sh ./gitInstallations/fonts/install.sh
+
+# install powerlevel9k for zsh
+cp -r ./gitInstallations/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# install the powerlevel9k theme:
+sed -i -e 's/ZSH_THEME\=\"robbyrussell\"/ZSH_THEME\=\"powerlevel9k\/powerlevel9k\"/' ~/.zshrc
+
+# install autojump
+./gitInstallations/autojump/install.py
+
+# install /.fzf
+./gitInstallations/fzf/install
+
 # install  homebrew: 
 # this will install xcode too if missing
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # then enter this to install brew cask
 brew cask
-# install these apps:
-brew cask install iterm2 spectacle google-chrome visual-studio-code karabiner-elements postman sequel-pro autojump
-# install zsh
-oh-my-zsh: sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-mkdir repos
-# install fonts:
-# clone
-cd ~
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-./fonts/install.sh
-# clean-up a bit
-sudo rm -rf fonts
-# this will install the powelevel9k theme for zsh
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# install these apps:
+brew install fzf
+
+$(brew --prefix)/opt/fzf/install
+
 
 
